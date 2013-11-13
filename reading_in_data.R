c84edb8327427a0a70b6eda5a4bd3f07d78b60bd
JUNFEI HUANG <effy.yao@gmail.com>
2013-11-13 14:19:07 -0800

    Update reading_in_data.R

--- reading_in_data.R
+++ reading_in_data.R
@@ -41,3 +41,16 @@ library(RODBC)
 ch <- odbcConnect('ph') #this is your dsn
 sample <- sqlQuery(ch, "SELECT top 100 * from [xx.xx.xx.xx].[XxxxXxx].[dbo].[DataWorks] dw")
 
+
+##
+titanic [1,5]  ##row1 and column 5
+
+titanic[5] ##return the 5th column
+
+survived <- titanic[titanic$Survived ==1,]  ## subset all rows where Survivied ==1
+not_survived <- titanic[titanic$Survived !=1,]
+
+titanic[1:10,]  ## return 10 rows
+titanic[1:10] ##return 10 columns
+
+titanic[-c(1,2,6)] ## return columns except column1 2 6
